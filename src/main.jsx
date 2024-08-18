022c0052e84d8528605707549d5df82b423eedb9
Rutvik Shinde <shinderutviksandeep@gmail.com>
2024-08-18 12:05:36 +0530

    small updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
@@ -10,6 +10,7 @@ Sentry.init({
   dsn: "https://3d674e61700e950a1c39278b4ad79566@o4507728932110336.ingest.us.sentry.io/4507793424384000",
   integrations: [
     Sentry.browserTracingIntegration(),
+    Sentry.browserProfilingIntegration(),
     Sentry.reactRouterV6BrowserTracingIntegration({
       useEffect: React.useEffect,
     }),
@@ -24,8 +25,8 @@ Sentry.init({
   replaysOnErrorSampleRate: 1.0,
 });
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "rutvik",
-    project: "iphone-landing-page"
-  })],
-
   build: {
-    sourcemap: true
-  }
-})
+    sourcemap: true,
+  },
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      org: "rutvik",
+      project: "iphone-landing-page",
+    }),
+  ],
+});
